1fa1921bceb44948e4450334990cb9825166bfec
Mohanraj Velusamy <mohanrajvelusamy@Mohanrajs-MacBook-Pro.local>
2024-12-12 21:59:55 +0530

    Fix new line test case

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,8 @@
 class StringCalculator
     def add(numbers)
       return 0 if numbers.empty?
-      return numbers.to_i if numbers =~ /^\d+$/
-      numbers.split(',').map(&:to_i).sum
+      
+      numbers.split(/,|\n/).map(&:to_i).sum
     end
 end
   
