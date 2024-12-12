594865cb9579c0730264dfb6e26f911fb2d4f93f
Mohanraj Velusamy <mohanrajvelusamy@Mohanrajs-MacBook-Pro.local>
2024-12-12 22:09:39 +0530

    Fix test case negative number exception

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,13 +2,17 @@ class StringCalculator
     def add(numbers)
       return 0 if numbers.empty?
 
+      delimiter = /,|\n/
       if numbers.start_with?("//")
         delimiter, numbers = numbers.split("\n", 2)
         delimiter = delimiter[2..]
-        numbers.split(delimiter).map(&:to_i).sum
-      else
-        numbers.split(/,|\n/).map(&:to_i).sum
       end
+  
+      nums = numbers.split(delimiter).map(&:to_i)
+      negatives = nums.select(&:negative?)
+      raise "negative numbers not allowed #{negatives.join(',')}" unless negatives.empty?
+  
+      nums.sum
     end
 end
   
