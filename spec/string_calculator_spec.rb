c8c217e11b4a81314e45a6c3e83adb567c530f09
Mohanraj Velusamy <mohanrajvelusamy@Mohanrajs-MacBook-Pro.local>
2024-12-12 21:53:39 +0530

    Handle new line test case

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,5 +13,9 @@ RSpec.describe StringCalculator do
     it 'returns the sum of two numbers' do
         expect(StringCalculator.new.add("1,5")).to eq(6)
     end
+
+    it 'handles newlines between numbers' do
+        expect(StringCalculator.new.add("1\n2,3")).to eq(6)
+    end
   end
 end
