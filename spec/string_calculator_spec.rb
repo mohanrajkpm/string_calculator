a982362dc134f45f45b48566dd61768c5281912b
Mohanraj Velusamy <mohanrajvelusamy@Mohanrajs-MacBook-Pro.local>
2024-12-12 21:45:26 +0530

    Initiate single string number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.new.add("")).to eq(0)
     end
+
+    it 'returns the number for a single number string' do
+        expect(StringCalculator.new.add("1")).to eq(1)
+    end
   end
 end
