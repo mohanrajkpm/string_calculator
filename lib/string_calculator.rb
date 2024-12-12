7e0b5f492a7f44b4a18672a18b84c03eb2bff2b9
Mohanraj Velusamy <mohanrajvelusamy@Mohanrajs-MacBook-Pro.local>
2024-12-12 21:52:05 +0530

    Pass test case two comma separated numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,7 @@ class StringCalculator
     def add(numbers)
       return 0 if numbers.empty?
       return numbers.to_i if numbers =~ /^\d+$/
+      numbers.split(',').map(&:to_i).sum
     end
 end
   
